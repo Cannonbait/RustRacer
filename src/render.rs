15af27cb5a066cde13f05809c67ccedd2b1e7a45
Ivar Josefsson <ivar.josefsson@nouw.com>
2020-05-13 17:00:26 +0200

    I tried

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,6 @@
 use super::*;
 
 pub fn render(objects: Vec<Object>, lights: Vec<Light>, options: Options) {
-    let mut buffer: Vec<u32> = vec![0; options.width * options.height];
-
     let mut window = Window::new(
         options.window_title.as_str(),
         options.width,
@@ -13,7 +11,7 @@ pub fn render(objects: Vec<Object>, lights: Vec<Light>, options: Options) {
         panic!("{}", e);
     });
 
-    let mut camera = Camera {
+    let camera = Camera {
         pos: Vector3f {
             x: 0.0,
             y: 0.0,
@@ -29,34 +27,35 @@ pub fn render(objects: Vec<Object>, lights: Vec<Light>, options: Options) {
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            generate_rays(&camera, &options);
-            break;
-            *i = 1; // write something more funny here!
-        }
-        break;
+        let rays = generate_rays(&camera, &options);
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
-            .update_with_buffer(&buffer, options.width, options.height)
+            .update_with_buffer(&buffer.concat(), options.width, options.height)
             .unwrap();
     }
 }
 
-fn generate_rays(camera: &Camera, options: &Options) {
+fn generate_rays(camera: &Camera, options: &Options) -> Vec<Ray> {
     let width = options.width as f32;
     let height = options.height as f32;
     let aspect_ratio = width / height;
     let fov = (options.fov as f32 / 2.0).tan();
-    let rays = Vec::<Ray>::new();
+    let mut rays = Vec::<Ray>::new();
     for j in 0..options.height {
         for i in 0..options.width {
             let ndc_x = (i as f32 + 0.5) / width;
             let ndc_y = (j as f32 + 0.5) / height;
             let screen_x = ndc_x * 2.0 - 1.0 * aspect_ratio * fov;
             let screen_y = 1.0 - ndc_y * 2.0 * fov;
-            let ray = Ray{
-                pos: camera.pos
-            }
+            rays.push(Ray {
+                pos: camera.pos,
+                dir: Vector3f {
+                    x: screen_x,
+                    y: screen_y,
+                    z: -1.0,
+                },
+            });
         }
     }
+    return rays;
 }
